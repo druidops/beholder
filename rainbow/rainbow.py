a922f6a24ebb0c0cc261dd8bc908ee7de36dc0fa
Loic Pefferkorn <loic@loicp.eu>
2015-01-29 19:56:24 +0100

    Update copyright date

--- rainbow/rainbow.py
+++ rainbow/rainbow.py
@@ -2,7 +2,7 @@
 # -*- coding: utf8 -*-
 
 """
-Copyright [2014] [beholder developers]
+Copyright [2014,2015] [beholder developers]
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
